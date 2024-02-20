7ae4d9be159b10af4326f7b1c788ab2525e0f512
Jay <horanghwnii@gmail.com>
2024-02-20 16:43:34 +0900

    chore: back to port number 5050

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(80);
+  await app.listen(5050);
 }
 bootstrap();
 // 3ErG0gLTsKZmYasZaLYz
